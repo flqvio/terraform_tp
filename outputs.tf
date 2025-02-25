d3becf7f56d341c313774d48ffdf7ac9537661cb
Flavio POGGIOLI <flavio.poggioli@etu.mines-ales.fr>
2025-02-25 10:08:43 +0100

    removing old-files

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "container_names" {
-  description = "Names of the spawned containers"
-  value       = docker_container.nginx[*].name
-}
-
-output "container_ports" {
-  description = "Ports of the spawned containers"
-  value       = [for i in range(var.num_containers) : var.starting_port + i]
-}
