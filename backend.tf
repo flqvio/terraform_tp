64bc996bca370d0b8e2e0c7c18d335dc1b92365c
Flavio POGGIOLI <flavio.poggioli@etu.mines-ales.fr>
2025-02-26 11:25:35 +0100

    adding lab5 + creation

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-qh2o7"
-    key            = "global/s3/student_19/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
-}
